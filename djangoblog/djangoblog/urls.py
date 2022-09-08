9e9877ce6d6532837e231b4266ab4728d5df88d7
Ken Vilar <kenvilar@gmail.com>
2022-09-08 23:58:08 +0800

    include the required imports and define a dictionary of sitemaps

--- djangoblog/djangoblog/urls.py
+++ djangoblog/djangoblog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
-from djangoblog.blog.sitemaps import PostSitemap
+from blog.sitemaps import PostSitemap
 
 sitemaps = {"posts": PostSitemap}
 
