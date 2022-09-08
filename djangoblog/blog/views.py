19982878ff6c07e5b604b8df41682286f15df224
Ken Vilar <kenvilar@gmail.com>
2022-09-08 17:59:41 +0800

    use the right page object

--- djangoblog/blog/views.py
+++ djangoblog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
 def post_list(request):
@@ -26,3 +27,10 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
