32ce304eb8064e9254bfea96900dde60d0a652cf
Ken Vilar <kenvilar@gmail.com>
2022-09-08 16:17:37 +0800

    Creating model managers

--- djangoblog/blog/models.py
+++ djangoblog/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status="published")
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ("draft", "Draft"),
@@ -18,6 +23,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ("-publish",)
