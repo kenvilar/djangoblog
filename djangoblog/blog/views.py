3406bb27e59cf25f7258725bb93d213fd2bc2cec
Ken Vilar <kenvilar@gmail.com>
2022-09-08 18:30:13 +0800

    Update post_share

--- djangoblog/blog/views.py
+++ djangoblog/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -32,13 +33,21 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status="published")
+    sent = False
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, "admin@kenvilar.com", [cd["to"]])
+            sent = True
         else:
             form = EmailPostForm
-        return render(request, "blog/post/share.html", {"post": post, "form": form})
+        return render(
+            request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+        )
 
 
 class PostListView(ListView):
