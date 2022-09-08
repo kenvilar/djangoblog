907537aa26e8eb2f3c72603bc88cb79b02acfb6d
Ken Vilar <kenvilar@gmail.com>
2022-09-08 22:33:20 +0800

    Add the TaggableManager manager to Post models.py

--- djangoblog/blog/models.py
+++ djangoblog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -24,6 +25,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+    tags = TaggableManager()
 
     objects = models.Manager()
     published = PublishedManager()
